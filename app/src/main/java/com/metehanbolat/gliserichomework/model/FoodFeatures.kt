f249ef973f4bc2d1278c942986bf764c0eabed8c
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-29 02:12:53 +0300

    Her bir yemek satırı oluşturuldu.

---
+++ app/src/main/java/com/metehanbolat/gliserichomework/model/FoodFeatures.kt
@@ -0,0 +1,8 @@
+package com.metehanbolat.gliserichomework.model
+
+data class FoodFeatures(
+    val name: String,
+    val glycemicIndex: String,
+    val carbohydrates: String,
+    val calories: String
+)
--- app/src/main/java/com/metehanbolat/gliserichomework/view/fragment/MainFragment.kt
+++ app/src/main/java/com/metehanbolat/gliserichomework/view/fragment/MainFragment.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.metehanbolat.gliserichomework.databinding.FragmentMainBinding
+import com.metehanbolat.gliserichomework.model.FoodFeatures
 import com.metehanbolat.gliserichomework.utils.Constants.URL
 import com.metehanbolat.gliserichomework.viewmodel.MainFragmentViewModel
 import kotlinx.coroutines.*
 import org.jsoup.Jsoup
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainFragment : Fragment() {
 
@@ -63,9 +66,33 @@ class MainFragment : Fragment() {
                 dataList.removeAll(tableList)
                 // Data List Featurestan arındırıldı.
                 dataList.removeAll(featuresList)
+                // En sondaki saçma şeyler temizlendi.
+                for (i in 1..3){
+                    dataList.removeAt(dataList.size - 1)
+                }
+                // Datalar saf halde
+                //println(dataList)
 
-                // Data List kullanıma açıldı.
-                println(dataList)
+                val foodNameList = ArrayList<String>()
+                val glycemicIndexList = ArrayList<String>()
+                val carbohydratesList = ArrayList<String>()
+                val caloriesList = ArrayList<String>()
+                // Yemeklerin özelliklerini ayrı ayrı listelere aldım.
+                for ((index, data) in dataList.withIndex()){
+                    when(index % 4){
+                        0 -> foodNameList.add(data)
+                        1 -> glycemicIndexList.add(data)
+                        2 -> carbohydratesList.add(data)
+                        3 -> caloriesList.add(data)
+                    }
+                }
+                val foodFeaturesList = ArrayList<FoodFeatures>()
+                for (i in 0 until foodNameList.size){
+                    val foodFeatures = FoodFeatures(foodNameList[i], glycemicIndexList[i], carbohydratesList[i], caloriesList[i])
+                    foodFeaturesList.add(foodFeatures)
+                }
+                println(foodFeaturesList)
+
             }
         }
 
