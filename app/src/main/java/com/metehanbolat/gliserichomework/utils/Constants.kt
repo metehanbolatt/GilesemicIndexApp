dad46b44387cb4917ee6dfd1ec7456a7b224b766
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-28 19:29:16 +0300

    Jsoup

---
+++ app/src/main/java/com/metehanbolat/gliserichomework/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.metehanbolat.gliserichomework.utils
+
+object Constants {
+
+    const val URL = "http://kolaydoktor.com/saglik-icin-yasam/diyet-ve-beslenme/besinlerin-glisemik-indeks-tablosu/0503/1"
+}
--- app/src/main/java/com/metehanbolat/gliserichomework/view/activity/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/gliserichomework/view/activity/MainActivity.kt
@@ -3,7 +3,14 @@ package com.metehanbolat.gliserichomework.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.metehanbolat.gliserichomework.databinding.ActivityMainBinding
+import com.metehanbolat.gliserichomework.utils.Constants.URL
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
 
+@DelicateCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
@@ -14,5 +21,18 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        GlobalScope.launch {
+            getData()
+        }
+    }
+
+    private fun getData() {
+        val document = Jsoup.connect(URL).get()
+        val element = document.select("tbody")
+        for (tr in element.select("tr")){
+            for (td in tr.select("td")){
+                println(td.text())
+            }
+        }
     }
 }
